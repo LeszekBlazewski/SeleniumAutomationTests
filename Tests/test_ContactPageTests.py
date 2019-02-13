395e36c14ab929d8026199b63b79e420e8a3555c
LeszekBlazewski <blazewski.leszek@gmail.com>
2019-02-13 11:04:41 +0100

    Refactored tests to follow py.test schema, removed unittest framework and implemented similar functions by py.test, added remote driver which is run in docker

--- Tests/test_ContactPageTests.py
+++ Tests/test_ContactPageTests.py
@@ -1,60 +1,72 @@
-from .BaseTestCase import BaseTestClass
 from ..pages.ContactPage import ContactPage
 from SeleniumAutomationTests.TestData import Test_Data
-from ..LocatorModes import LocatorMode
-from ..DriverSettings import Driver_Settings
+from ..Cookies import cookies
 import pytest
 
 
-class ContactPageTests(BaseTestClass):
+class TestContactPage():
 
-    def setUp(self):
-        super(ContactPageTests, self).setUp()
-        self.navigate_to_page(Driver_Settings['Contact_page_URL'])
-
-    def test_send_message_valid_data(self):
-        contact_page_obj = ContactPage(self.driver)
+    @pytest.mark.parametrize('setUp_test_information_for_zelenium',
+                             ["test_send_message_valid_data"])
+    def test_send_message_valid_data(self, driver_init):
+        contact_page_obj = ContactPage(driver_init)
         contact_page_obj.sendMessage(
             Test_Data['Valid_Message_header'], Test_Data['Valid_Email'],
             Test_Data['Valid_order_reference'], 'test message',
             Test_Data['Valid_attachment_path'])
         assert contact_page_obj.verify_success_messag_popup()
+        driver_init.add_cookie(cookies[1])
 
-    def test_send_message_blank_email(self):
-        contact_page_obj = ContactPage(self.driver)
+    @pytest.mark.parametrize('setUp_test_information_for_zelenium',
+                             ["test_send_message_blank_email"])
+    def test_send_message_blank_email(self, driver_init):
+        contact_page_obj = ContactPage(driver_init)
         contact_page_obj.sendMessage(
             Test_Data['Valid_Message_header'], '',
             Test_Data['Valid_order_reference'], 'test message',
             Test_Data['Valid_attachment_path'])
         assert contact_page_obj.verify_failure_message_popup()
+        driver_init.add_cookie(cookies[1])
 
     @pytest.mark.xfail
-    def test_send_message_blank_order_reference(self):
-        contact_page_obj = ContactPage(self.driver)
+    @pytest.mark.parametrize('setUp_test_information_for_zelenium',
+                             ["test_send_message_blank_order_reference"])
+    def test_send_message_blank_order_reference(self, driver_init):
+        contact_page_obj = ContactPage(driver_init)
         contact_page_obj.sendMessage(
             Test_Data['Valid_Message_header'], Test_Data['Valid_Email'],
             '', 'test message', Test_Data['Valid_attachment_path'])
         assert contact_page_obj.verify_failure_message_popup()
+        driver_init.add_cookie(cookies[1])
 
-    def test_send_message_blank_message_text(self):
-        contact_page_obj = ContactPage(self.driver)
+    @pytest.mark.parametrize('setUp_test_information_for_zelenium',
+                             ["test_send_message_blank_message_text"])
+    def test_send_message_blank_message_text(self, driver_init):
+        contact_page_obj = ContactPage(driver_init)
         contact_page_obj.sendMessage(
             Test_Data['Valid_Message_header'], Test_Data['Valid_Email'],
             Test_Data['Valid_order_reference'], '',
             Test_Data['Valid_attachment_path'])
         assert contact_page_obj.verify_failure_message_popup()
+        driver_init.add_cookie(cookies[1])
 
-    def test_send_message_invalid_email(self):
-        contact_page_obj = ContactPage(self.driver)
+    @pytest.mark.parametrize('setUp_test_information_for_zelenium',
+                             ["test_send_message_invalid_email"])
+    def test_send_message_invalid_email(self, driver_init):
+        contact_page_obj = ContactPage(driver_init)
         contact_page_obj.sendMessage(
             Test_Data['Valid_Message_header'], Test_Data['Invalid_Email'],
             Test_Data['Valid_order_reference'], 'test message',
             Test_Data['Valid_attachment_path'])
         assert contact_page_obj.verify_failure_message_popup()
+        driver_init.add_cookie(cookies[1])
 
-    def test_send_message_no_attachment(self):
-        contact_page_obj = ContactPage(self.driver)
+    @pytest.mark.parametrize('setUp_test_information_for_zelenium',
+                             ["test_send_message_no_attachment"])
+    def test_send_message_no_attachment(self, driver_init):
+        contact_page_obj = ContactPage(driver_init)
         contact_page_obj.sendMessage(
             Test_Data['Valid_Message_header'], Test_Data['Valid_Email'],
             Test_Data['Valid_order_reference'], 'test message', '')
         assert contact_page_obj.verify_success_messag_popup()
+        driver_init.add_cookie(cookies[1])
