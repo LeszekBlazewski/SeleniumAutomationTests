31ee5350924dff5b1da51679124adf887fe0d573
LeszekBlazewski <blazewski.leszek@gmail.com>
2019-02-21 15:51:46 +0100

    Added tests for contact page, and locators for next 2 page objects

--- Locators.py
+++ Locators.py
@@ -12,3 +12,56 @@ class ContactPageLocators():
     SEND_BUTTON = (By.ID, 'submitMessage')
     SUCCESS_ALERT = (By.CSS_SELECTOR, '.alert.alert-success')
     FAILURE_ALERT = (By.CSS_SELECTOR, '.alert.alert-danger')
+
+
+class LoginPageLocators():
+    """Conatins all of the locators used to identify objects on Login page"""
+    LOGIN_HEADER = (By.XPATH, "//*[text()='Authentication']")
+    EMAIL_FIELD_REGISTRATION = (By.ID, "email_create")
+    EMAIL_FIELD_LOGIN = (By.ID, "email")
+    CREATE_ACCOUNT_BUTTON = (By.ID, "SubmitCreate")
+    SIGN_IN_BUTTON = (By.ID, "SubmitLogin")
+    PASSWORD_FIELD = (By.ID, "passwd")
+    FORGOT_PASSWORD_LINK = (By.CSS_SELECTOR, ".lost_password.form-group")
+    FAILURE_ALERT_WRONG_EMAIL_CREATE_ACCOUNT = (
+        By.XPATH, "//*[contains(text(), 'Invalid email address')]")
+    FAILURE_ALERT_WRONG_EMAIL_LOGIN = (
+        By.XPATH, "//*[contains(text(), 'Invalid email')]")
+    FAILURE_ALERT_WRONG_PASSWORD = (
+        By.XPATH, "//*[contains(text(), 'Authentication failed')]")
+    FAILURE_ALERT_BLANK_EMAIL_LOGIN = (
+        By.XPATH, "//*[contains(text(), 'email address required')]")
+    FAILURE_ALERT_BLANK_PASSWORD_LOGIN = (
+        By.XPATH, "//*[contains(text(), 'Password is required')]")
+    FAILURE_ALERT_TAKEN_EMAIL = (
+        By.XPATH, "//*[contains(text(), 'already been registered')]")
+    FOROGT_PASSWORD_BANNER = (
+        By.XPATH, "// *[contains(text(), 'Forgot your password')]")
+
+
+class RegistrationPageLocators():
+    """Contains all of the locators used to indentify objects on Registration page"""
+    REGISTRATION_HEADER = (By.XPATH, "//*[text()='Create an account']")
+    GENDER_SELECT_BUTTON_MR = (By.ID, 'id_gender1')
+    GENDER_SELECT_BUTTON_MRS = (By.ID, 'id_gender2')
+    FIRST_NAME_FIELD = (By.ID, 'customer_firstname')
+    LAST_NAME_FIELD = (By.ID, 'customer_lastname')
+    EMAIL_FIELD = (By.ID, 'email')
+    PASSWORD_FIELD = (By.ID, "passwd")
+    DAYS_FIELD = (By.ID, 'days')
+    MONTHS_FIELD = (By.ID, 'months')
+    YEARS_FIELD = (By.ID, 'years')
+    NEWSLETTER_TICK = (By.ID, 'newsletter')
+    SPECIAL_OFFERS_TICK = (By.ID, 'optin')
+    COMPANY_FIELD = (By.ID, 'company')
+    ADDRESS_STREET_FIELD = (By.ID, 'address1')
+    ADDRESS_APARTMENT_FIELD = (By.ID, 'address2')
+    CITY_FIELD = (By.ID, 'city')
+    STATE_FIELD = (By.ID, 'id_state')
+    POSTAL_CODE_FIELD = (By.ID, 'postcode')
+    COUNTRY_FIELD = (By.ID, 'id_country')
+    OTHER_INFORMATION_FIELD = (By.ID, 'other')
+    HOME_PHONE_FIELD = (By.ID, 'phone')
+    MOBILE_PHONE_FIELD = (By.ID, 'phone_mobile')
+    ALIAS_FIELD = (By.ID, 'alias')
+    REGISTER_BUTTON = (By.ID, 'submitAccount')
---
+++ Tests/ContactPageTests.py
@@ -0,0 +1,72 @@
+from ..pages.ContactPage import ContactPage
+from SeleniumAutomationTests.TestData import TestData
+from ..Cookies import cookies
+import pytest
+
+
+class TestContactPage():
+    """ Contains tests regarding Contact page """
+    @pytest.mark.parametrize('setUp_test_information_for_zelenium',
+                             ["test_send_message_valid_data"])
+    def test_send_message_valid_data(self, driver_init):
+        contact_page_obj = ContactPage(driver_init)
+        contact_page_obj.sendMessage(
+            TestData.VALID_MESSAGE_HEADER, TestData.VALID_EMAIL,
+            TestData.VALID_ORDER_REFERENCE, 'test message',
+            TestData.VALID_ATTACHMENT_PATH)
+        assert contact_page_obj.verify_messag_popup("success")
+        driver_init.add_cookie(cookies[1])
+
+    @pytest.mark.parametrize('setUp_test_information_for_zelenium',
+                             ["test_send_message_blank_email"])
+    def test_send_message_blank_email(self, driver_init):
+        contact_page_obj = ContactPage(driver_init)
+        contact_page_obj.sendMessage(
+            TestData.VALID_MESSAGE_HEADER, '',
+            TestData.VALID_ORDER_REFERENCE, 'test message',
+            TestData.VALID_ATTACHMENT_PATH)
+        assert contact_page_obj.verify_messag_popup("failure")
+        driver_init.add_cookie(cookies[1])
+
+    @pytest.mark.xfail
+    @pytest.mark.parametrize('setUp_test_information_for_zelenium',
+                             ["test_send_message_blank_order_reference"])
+    def test_send_message_blank_order_reference(self, driver_init):
+        contact_page_obj = ContactPage(driver_init)
+        contact_page_obj.sendMessage(
+            TestData.VALID_MESSAGE_HEADER, TestData.VALID_EMAIL,
+            '', 'test message', TestData.VALID_ATTACHMENT_PATH)
+        assert contact_page_obj.verify_messag_popup("failure")
+        driver_init.add_cookie(cookies[1])
+
+    @pytest.mark.parametrize('setUp_test_information_for_zelenium',
+                             ["test_send_message_blank_message_text"])
+    def test_send_message_blank_message_text(self, driver_init):
+        contact_page_obj = ContactPage(driver_init)
+        contact_page_obj.sendMessage(
+            TestData.VALID_MESSAGE_HEADER, TestData.VALID_EMAIL,
+            TestData.VALID_ORDER_REFERENCE, '',
+            TestData.VALID_ATTACHMENT_PATH)
+        assert contact_page_obj.verify_messag_popup("failure")
+        driver_init.add_cookie(cookies[1])
+
+    @pytest.mark.parametrize('setUp_test_information_for_zelenium',
+                             ["test_send_message_invalid_email"])
+    def test_send_message_invalid_email(self, driver_init):
+        contact_page_obj = ContactPage(driver_init)
+        contact_page_obj.sendMessage(
+            TestData.VALID_MESSAGE_HEADER, TestData.INVALID_EMAIL,
+            TestData.VALID_ORDER_REFERENCE, 'test message',
+            TestData.VALID_ATTACHMENT_PATH)
+        assert contact_page_obj.verify_messag_popup("failure")
+        driver_init.add_cookie(cookies[1])
+
+    @pytest.mark.parametrize('setUp_test_information_for_zelenium',
+                             ["test_send_message_no_attachment"])
+    def test_send_message_no_attachment(self, driver_init):
+        contact_page_obj = ContactPage(driver_init)
+        contact_page_obj.sendMessage(
+            TestData.VALID_MESSAGE_HEADER, TestData.VALID_EMAIL,
+            TestData.VALID_ORDER_REFERENCE, 'test message', '')
+        assert contact_page_obj.verify_messag_popup("success")
+        driver_init.add_cookie(cookies[1])
