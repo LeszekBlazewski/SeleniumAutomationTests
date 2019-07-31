9d6fb83198769d04fe3d90ee48b2082fd2a5c31b
LeszekBlazewski <blazewski.leszek@gmail.com>
2019-07-31 21:25:24 +0200

    Fixed w3c errors for chrome

--- Tests/conftest.py
+++ Tests/conftest.py
@@ -10,10 +10,14 @@ from ..TestData import DataGenerator
 def driver_init(request, setUp_test_information_for_zelenium):
     selenium_hub_url = 'http://localhost:4444/wd/hub'
     capabilities = None
+    options = None
     if request.param == "firefox":
         capabilities = DesiredCapabilities.FIREFOX
+        options = webdriver.FirefoxOptions()
     elif request.param == "chrome":
         capabilities = DesiredCapabilities.CHROME
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('w3c', False)
     else:
         raise Exception(
             'Browser could not be specified\n Check test params !')
@@ -22,7 +26,7 @@ def driver_init(request, setUp_test_information_for_zelenium):
     capabilities['idleTimeout'] = 90
     capabilities['recordVideo'] = False
     capabilities['testFileNameTemplate'] = "{testName}_{platform}_{browser}_{testStatus}"
-    web_driver = webdriver.Remote(selenium_hub_url, capabilities)
+    web_driver = webdriver.Remote(selenium_hub_url, capabilities,None,None,False,None,options) 
     web_driver.maximize_window()
     yield web_driver
     web_driver.quit()
